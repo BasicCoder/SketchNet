3be032712bce22cb2c7832b1a5d7c6529b84e19b
BasicCoder <abasiccoder@gmail.com>
2017-06-02 17:15:40 +0800

    modify ReadData methods

---
+++ ReadData_np.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import tensorflow as tf 
+import numpy as np 
+import os 
+import json 
+
+test_images_name = []
+test_sketchs_name = []
+test_triplets = []
+
+train_images_name = []
+train_images_sketch = []
+train_triplets = []
+
+def LoadTriplets(filename):
+    with open(filename, encoding='utf-8') as f:
+        info = json.load(f)
+        test_images_name = info['test']['images']
+        test_sketchs_name = info['test']['sketches']
+        test_triplets = info['test']['triplets']
+
+        train_images_name = info['train']['images']
+        train_sketchs_name = info['train']['sketches']
+        train_triplets = info['train']['triplets']
+
+    #print(len(train_triplets))
+    #print(train_images_name[0])
+    #print(train_sketchs_name[0])
+    #print(train_triplets[0][0])
+    return (test_images_name, test_sketchs_name, test_triplets, train_images_name, train_sketchs_name, train_triplets)
+
+
+def get_image(image_path):
+    img = tf.image.convert_image_dtype( tf.image.decode_jpeg( tf.read_file(image_path), channels = 3),  dtype=tf.uint8 )
+    img = tf.reshape(img, [256, 256, 3])
+    return img
+
+def get_sketch(image_path):
+    img = tf.image.convert_image_dtype( tf.image.decode_png( tf.read_file(image_path), channels = 3), dtype=tf.uint8)
+    img = tf.reshape(img, [256, 256, 3])
+    return img
+
+def ReadData(sess, batch_size = 128):
+
+    shoes_annotation = r'../shoes_annotation.json'
+    print(r'Loading ' + shoes_annotation + r'...')
+    test_images_name, test_sketchs_name, test_triplets, train_images_name, train_sketchs_name, train_triplets = LoadTriplets(shoes_annotation)
+
+    # Read all images
+    shoes_images = []
+    data_path = r'E:\work\Python\shoes\train\images'
+    print('Loading train images ...')
+    for img in train_images_name:
+        a = get_image(data_path + '\\' + img)
+        shoes_images.append(a)
+    print(len(shoes_images))
+    #print(shoes_images[0])
+
+    # Read all sketchs
+    shoes_sketchs = []
+    data_path = r'E:\work\Python\shoes\train\sketches'
+    print('Loading train sketches ...')
+    for img in train_sketchs_name:
+        b = get_sketch(data_path + '\\' + img)
+        shoes_sketchs.append(b)
+    print(len(shoes_sketchs))
+    #print(shoes_sketchs[0])
+
+    # yield batch data
+    for i in range(len(shoes_images) * 45):
+        t0 = int(i / 45)
+        t1 = i % 45
+
+        t = train_triplets[t0][t1]
+        t2 = t[0]
+        t3 = t[1]
+
+        s = None
+        ipos = None
+        ineg = None
+
+        if s is None:
+            s = shoes_sketchs[t0]
+        else:
+            s = tf.concat(0, [s, shoes_sketchs[t0]])
+        
+        if ipos is None:
+            ipos = shoes_images[t2]
+        else:
+            ipos = tf.concat(0, [ipos, shoes_images[t2]])
+
+        if ineg is None:
+            ineg = shoes_images[t3]
+        else:
+            ineg = tf.concat(0, [ineg, shoes_images[t3]])
+
+        if i % batch_size == 0:
+            yield s, ipos, ineg
+            s = None
+            ipos = None
+            ineg = None
+        
+
+
+if __name__ == '__main__':
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        a = ReadData(sess)
+        next(a)
+        next(a)
