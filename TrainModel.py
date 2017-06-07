3ca0b662534b683b2d84f8a048febc26fb2277ae
BasicCoder <abasiccoder@gmail.com>
2017-06-07 15:16:32 +0800

    fixbug

--- TrainModel.py
+++ TrainModel.py
@@ -72,8 +72,8 @@ def run_training():
     keep_prob = tf.placeholder(tf.float32)
 
     sketch_dense = SketchNet(sketchs_placeholder, _weights = sketch_weights, _biases = sketch_biases, dropout_prob = keep_prob)
-    image_pos_dense = ImageNetPos(images_neg_placeholder, _weights = weights, _biases = biases, dropout_prob = keep_prob)
-    image_neg_dense = ImageNetNeg(images_pos_placeholder, _weights = weights, _biases = biases, dropout_prob = keep_prob)
+    image_pos_dense = ImageNetPos(images_neg_placeholder, _weights = image_weights, _biases = image_biases, dropout_prob = keep_prob)
+    image_neg_dense = ImageNetNeg(images_pos_placeholder, _weights = image_weights, _biases = image_biases, dropout_prob = keep_prob)
 
     # Euclidean Distance
     dist_pos = EuclideanDist(sketch_dense, image_pos_dense)
