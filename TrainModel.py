a27923561f007af4b0f778f4b0fee373c7110d34
BasicCoder <abasiccoder@gmail.com>
2017-06-09 10:57:02 +0800

    debug Model

--- TrainModel.py
+++ TrainModel.py
@@ -29,7 +29,7 @@ image_weights = {
     'wc3': tf.Variable(tf.random_normal([3, 3, 128, 256])),
     'wc4': tf.Variable(tf.random_normal([3, 3, 256, 256])),
     'wc5': tf.Variable(tf.random_normal([3, 3, 256, 256])),
-    'wd1': tf.Variable(tf.random_normal([7*7*256, 512])), 
+    'wd1': tf.Variable(tf.random_normal([8*8*256, 512])), 
     'wd2': tf.Variable(tf.random_normal([512, 256])),
 }
 
